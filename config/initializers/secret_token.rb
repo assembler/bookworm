a42b72a3c78a84e5983b959bc18e6369b4dbc0ca
Milovan Zogovic <milovan.zogovic@gmail.com>
2013-05-18 07:53:56 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookworm::Application.config.secret_token = '9a068dc88e717dfe8a10627c591472cd1fff7d8cfb856e1606448c5bf888fd29549fbf597ee447605b09e55b7cbd9ed6cc7f4290bed50aaad899119ce9e7b6da'
